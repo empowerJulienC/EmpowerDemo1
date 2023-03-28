fc3d6376f8b8ab4384bc43c4ddfe5d7601748b4f
jcaldana <jcaldana@LPT-VOOO-220049>
2023-03-28 15:55:43 +0200

    Add project files.

---
+++ EmpowerDemo1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("38F90AD6-6369-4F4E-81E7-89165ACBBC70")]
+[assembly: AssemblyVersion("1.0.0.0")]
